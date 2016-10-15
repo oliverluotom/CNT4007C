e10a7fb19481a0ded75b0f6b5b6211ec7b42478f
Joshua Kirstein <joshuakirstein@Joshuas-MacBook-Pro.local>
2016-10-14 22:50:25 -0400

    Added client connecting/listening to peers.

--- src/main/java/com/networking/model/Client.java
+++ src/main/java/com/networking/model/Client.java
@@ -2,6 +2,8 @@ package com.networking.model;
 
 import com.networking.config.*;
 
+import java.io.*;
+import java.net.*;
 import java.util.*;
 
 /**
@@ -32,14 +34,72 @@ public class Client implements Runnable {
         // will be called by a timer asynchronously
     }
 
-    public synchronized void addPeer(Peer p) {
-        peers.add(p);
+    public synchronized void addPeer(Peer peer) {
+        peers.add(peer);
+    }
+
+    private synchronized void connect(PeerConfig pConfig) throws IOException {
+        // open socket to pConfig.
+        Socket socket = new Socket(pConfig.getHost(), pConfig.getPort());
+        addPeer(new Peer(socket, this));
     }
 
     public void run() {
         System.out.println("Starting client with ID <" + getClientID() + "> on port <" + clientCfg.getPort() + ">");
-        // start data/random unchoker
         // connect to lower peers
+        for (PeerConfig pConfig : PeerConfig.PEER_CONFIGS) {
+            if (pConfig.getPeerID() < clientCfg.getPeerID()) {
+                try {
+                    connect(pConfig);
+                } catch (IOException ex) {
+                    // fatally exit if we can't connect to a prior peer
+                    System.out.println("Error connecting to peer <" + pConfig.getPeerID() + ">, terminating.");
+                    System.exit(1);
+                }
+            }
+        }
+        // start data choker
+        new Thread("Data Unchoker") {
+            long lastChoke = 0;
+            public void run() {
+                while (true) {
+                    long curTime = System.currentTimeMillis();
+                    if ((curTime-lastChoke) >= 1000*CommonConfig.getDataUnchokeInterval()) {
+                        lastChoke = curTime;
+                        dataUnchoke();
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) { continue; }
+                }
+            }
+        }.start();
+        // start random unchoker
+        new Thread("Random Unchoker") {
+            long lastChoke = 0;
+            public void run() {
+                while (true) {
+                    long curTime = System.currentTimeMillis();
+                    if ((curTime-lastChoke) >= 1000*CommonConfig.getRandomUnchokeInterval()) {
+                        lastChoke = curTime;
+                        randomUnchoke();
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) { continue; }
+                }
+            }
+        }.start();
         // listen for higher peers
+        try {
+            ServerSocket server = new ServerSocket(clientCfg.getPort());
+            do {
+                Socket socket = server.accept();
+                addPeer(new Peer(socket, this));
+            } while(true);
+        } catch (Exception ex) {
+            System.out.println("Error running server socket, terminating.");
+            System.exit(1);
+        }
     }
 }
